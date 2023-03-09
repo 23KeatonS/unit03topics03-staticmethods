bb9baffc7508f7d4d0ce6061a5f9a2904adace47
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-03-09 12:19:02 -0600

    finished hms started colon

--- TimeDuration.java
+++ TimeDuration.java
@@ -18,63 +18,95 @@ public class TimeDuration {
 
 
 
-    public static TimeDuration parseFromString(String str){
-        
 
+
+
+    public static int NumBeforeLet(String str, String letter){
+            int EndIndex = str.indexOf(letter);
+            while(!stringIsNumber(str.substring(EndIndex,EndIndex+1))){
+                EndIndex--;
+            }
+            int startIndex = EndIndex;
+            while(stringIsNumber(str.substring(startIndex,startIndex+1))){
+                if(startIndex!=0){
+                    startIndex--;
+                }else{
+                    break;
+                }
+            }
+            if(startIndex!=0){
+                startIndex++;
+            }
+            return Integer.parseInt(str.substring(startIndex,EndIndex+1));
+        
+    }
+
+
+    public static int[] NumAfterColon(String str){
+        int startIndex = str.lastIndexOf(":");
+        int colonIndex = startIndex;
+        while(!stringIsNumber(str.substring(startIndex,startIndex+1))){
+            startIndex++;
+        }
+        int endIndex = startIndex;
+        while(stringIsNumber(str.substring(endIndex,endIndex+1))){
+            if(endIndex!=str.length()-1){
+                endIndex++;
+        }else{
+            break;
+        }
+
+    }
+    if(endIndex==str.length()-1){
+        endIndex++;
+    }
+    return new int[] {Integer.parseInt(str.substring(startIndex,endIndex)),colonIndex};
+}
+
+
+
+
+    public static TimeDuration parseFromString(String str){
+    
         int realHours = 0;
         int realMinutes = 0;
         int realSeconds = 0;
 
         if(str.indexOf("h")!=-1){
-            int hoursEndIndex = str.indexOf("h");
-            while(!stringIsNumber(str.substring(hoursEndIndex, hoursEndIndex+1))){
-                hoursEndIndex--;
-            }
-            hoursEndIndex++;
-            int hoursStartIndex = hoursEndIndex-1;
-            while(stringIsNumber(str.substring(hoursStartIndex,hoursStartIndex+1))){
-                hoursStartIndex--;
-            }
-            hoursStartIndex++;
-            realHours = Integer.parseInt(str.substring(hoursStartIndex,hoursEndIndex));
+            realHours = NumBeforeLet(str, "h");
         }
-
         if(str.indexOf("m")!=-1){
-            int minutesEndIndex = str.indexOf("m");
-            while(!stringIsNumber(str.substring(minutesEndIndex, minutesEndIndex+1))){
-                minutesEndIndex--;
-            }
-            minutesEndIndex++;
-            int minutesStartIndex = minutesEndIndex-1;
-            while(stringIsNumber(str.substring(minutesStartIndex,minutesStartIndex+1))){
-                minutesStartIndex--;
-            }
-            minutesStartIndex++;
-            realMinutes = Integer.parseInt(str.substring(minutesStartIndex,minutesEndIndex));
+            realMinutes = NumBeforeLet(str, "m");
+        }
+        if(str.indexOf("s")!=-1){
+            realSeconds = NumBeforeLet(str, "s");
         }
 
+        int lastIndex = str.length()-1;
+        if(str.indexOf(":")!=-1){
+            int [] results = NumAfterColon(str);
+            realSeconds = results[0];
+            lastIndex = results[1];
 
-        if(str.indexOf("s")!=-1){
-            int secondsEndIndex = str.indexOf("s");
-            while(!stringIsNumber(str.substring(secondsEndIndex, secondsEndIndex+1))){
-                secondsEndIndex--;
+        }
+        if(str.indexOf(":")!=-1){
+            int[] results = NumAfterColon(str.substring(0,lastIndex));
+            realMinutes = results[0];
+            lastIndex = results[1];
+            if(stringIsNumber(str.substring(0,1))){
+                realHours = Integer.parseInt(str.substring(0,lastIndex));
             }
-            System.out.println(secondsEndIndex);
-            secondsEndIndex++;
-            int secondsStartIndex = secondsEndIndex-1;
-            while(stringIsNumber(str.substring(secondsStartIndex,secondsStartIndex+1))){
-                secondsStartIndex--;
-            }
-            System.out.println(secondsStartIndex);
-            secondsStartIndex++;
-            realSeconds = Integer.parseInt(str.substring(secondsStartIndex,secondsEndIndex));
-            System.out.println(realSeconds);
             
         }
-        
+
+
+
         return new TimeDuration(realHours,realMinutes,realSeconds);
+
+
     }
 
+        
 
 
     private static boolean stringIsNumber(String s) {
@@ -110,10 +142,16 @@ public class TimeDuration {
 
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a time: ");
         String userInput = scanner.nextLine();
         TimeDuration time1 = parseFromString(userInput);
+        
+        System.out.println(time1.hours);
+        System.out.println(time1.minutes);
+        System.out.println(time1.seconds);
+        System.out.println(NumAfterColon(":1234:123:12")[0]);
 
         
     }
